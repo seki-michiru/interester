1c62bd0a3a16861787d0b6adc7f6e3c76ea370cc
seki-michiru <66760513+seki-michiru@users.noreply.github.com>
2020-06-11 11:43:52 +0900

    Add files via upload

---
+++ Interester/src/main/java/jp/co/example/InteresterApplication.java
@@ -0,0 +1,13 @@
+package jp.co.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteresterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteresterApplication.class, args);
+	}
+
+}
